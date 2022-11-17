e8640e2573900b8bc67ac15d014855ba12f73368
Jpschnabel177 <jpschnabel96@gmail.com>
2022-11-17 16:01:05 -0500

    finished

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -9,7 +9,7 @@ const iApi = axios.create({
 router.get("/characters", (req, res, next) => {
     iApi.get("/characters")
     .then(responseFromAPI => {
-         console.log(responseFromAPI)
+         //console.log(responseFromAPI)
         res.render("characters/list-characters", { characters: responseFromAPI.data });
     })
     .catch(err => console.error(err))
@@ -30,18 +30,52 @@ router.get("/characters/:id", (req, res, next) => {
 
 
 router.post("/characters/create", (req, res, next) => {
-    console.log(req.body)
+    
     if(req.body.debt === "true"){
         req.body.debt = true
     }else{
         req.body.debt = false
     }
+    
     iApi.post(`/characters/`, req.body)
     .then(() => {;
         res.redirect("/characters")
     })
     .catch(err => console.error(err))
 });
+
+router.get("/characters/:id/edit", (req,res,next) => {
+     iApi.get(`/characters/${req.params.id}`)
+    .then(resp => {
+        // console.log("details: ", responseFromAPI.data)
+        res.render("characters/edit-character", { character: resp.data })
+    })
+    .catch(err => console.error(err))
+    
+})
+
+router.post("/characters/:id/update", (req, res, next) => {
+    
+    if(req.body.debt === "true"){
+        req.body.debt = true
+    }else{
+        req.body.debt = false
+    }
+    
+    //api.put(`/characters/${characterId}`, characterInfo)
+    iApi.put(`/characters/${req.params.id}`, req.body)
+    .then((char) => {
+        res.redirect("/characters")
+    })
+    .catch(err => console.error(err))
+});
+
+//delete api.delete(`/characters/${characterId}`
+router.post("/characters/:id/delete", (req,res,next) => {
+    iApi.delete(`/characters/${req.params.id}`).then(()=>{
+        res.redirect("/characters")
+    })
+})
 
 
 module.exports = router;
