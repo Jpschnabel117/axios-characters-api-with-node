517777f0b8626ca050d52d8fa7d6c495a2430440
Jpschnabel177 <jpschnabel96@gmail.com>
2022-11-17 12:20:17 -0500

    halfway

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -1,19 +1,26 @@
 const router = require("express").Router();
 const axios = require("axios");
 
+const iApi = axios.create({
+  baseURL: 'https://ih-crud-api.herokuapp.com'
+});
+
 /* GET home page */
 router.get("/characters", (req, res, next) => {
-    axios.get("https://ih-crud-api.herokuapp.com/characters")
+    iApi.get("/characters")
     .then(responseFromAPI => {
-        // console.log(responseFromAPI)
+         console.log(responseFromAPI)
         res.render("characters/list-characters", { characters: responseFromAPI.data });
     })
     .catch(err => console.error(err))
 });
 
+router.get("/characters/create", (req, res, next) => {
+    res.render('characters/create-character')
+});
 
 router.get("/characters/:id", (req, res, next) => {
-    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    iApi.get(`/characters/${req.params.id}`)
     .then(responseFromAPI => {
         // console.log("details: ", responseFromAPI.data)
         res.render("characters/details-character", { character: responseFromAPI.data });
@@ -21,7 +28,21 @@ router.get("/characters/:id", (req, res, next) => {
     .catch(err => console.error(err))
 });
 
+
+router.post("/characters/create", (req, res, next) => {
+    console.log(req.body)
+    if(req.body.debt === "true"){
+        req.body.debt = true
+    }else{
+        req.body.debt = false
+    }
+    iApi.post(`/characters/`, req.body)
+    .then(() => {;
+        res.redirect("/characters")
+    })
+    .catch(err => console.error(err))
+});
+
+
 module.exports = router;
 
-
-// https://ih-crud-api.herokuapp.com/characters
